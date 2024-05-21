5568dde7f3df383c29c58bc3e3983765aa0d25a3
Denis Belov <den-belov@yandex.ru>
2024-05-21 15:52:42 +0300

    Test install

--- readme.md
+++ readme.md
@@ -2,6 +2,6 @@
    [плагин для Google Chrome](https://chromewebstore.google.com/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=ru&pli=1).
    [плагин для Mozilla Firefox](https://addons.mozilla.org/ru/firefox/addon/tampermonkey/).
 
-2. Ткнуть ссылку: [Install yandexTrackerTimer](https://raw.githubusercontent.com/denyWhite/yandexTrackerTimer/main/install.js)
+2. Ткнуть ссылку: [Install yandexTrackerTimer](https://raw.githubusercontent.com/denyWhite/yandexTrackerTimer/main/yandexTrackerTimer.js)
 
 3. Проверяем работу трекера.
--- yandexTrackerTimer.js
+++ yandexTrackerTimer.js
@@ -1,4 +1,18 @@
-let div = document.createElement('div');
-div.className = "alert";
-div.innerHTML = "<strong>Всем привет!</strong> Вы прочитали важное сообщение.";
-document.body.append(div);
+// ==UserScript==
+// @name         Yandex Tracker Timer
+// @version      1.0
+// @description  Simple timer for Yandex Tracker
+// @author       Denis Belov
+
+// @match       https://tracker.yandex.ru/agile/board/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let div = document.createElement('div');
+    div.className = "alert";
+    div.innerHTML = "<strong>Всем привет!</strong> Вы прочитали важное сообщение.";
+    document.body.append(div);
+})();
