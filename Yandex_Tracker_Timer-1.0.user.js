cc5eacca8c995249d7a1b0096a2ae5ee53a0f160
Denis Belov <den-belov@yandex.ru>
2024-05-21 15:56:06 +0300

    Test install

---
+++ Yandex_Tracker_Timer-1.0.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Yandex_Tracker_Timer
+// @version      1.0
+// @description  Simple timer for Yandex Tracker
+// @author       Denis Belov
+
+// @match       https://tracker.yandex.ru/agile/board/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    alert(1);
+})();
